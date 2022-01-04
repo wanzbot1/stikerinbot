60a361e6170d4c065e1945e0bc3522cd9709ae82
wanzbot1 <96588684+wanzbot1@users.noreply.github.com>
2022-01-04 10:10:20 +0800

    Update tebaklirik.js

--- plugins/tebaklirik.js
+++ plugins/tebaklirik.js
@@ -18,7 +18,7 @@ Ketik ${usedPrefix}teli untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.tebaklirik[id] = [
-        await conn.sendButton(m.chat, caption, '© stikerin', 'Bantuan', `.teli`, m),
+        await conn.sendButton(m.chat, caption, '© icebotz', 'Bantuan', `.teli`, m),
         json, poin,
         setTimeout(() => {
             if (conn.tebaklirik[id]) conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, '© stikerin', 'Tebak Lirik', `.tebaklirik`, conn.tebaklirik[id][0])
