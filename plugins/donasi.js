2b756d00e1a2348d1d57951279b0bbc47201039e
wanzbot1 <96588684+wanzbot1@users.noreply.github.com>
2022-01-04 09:55:03 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, fla + 'donasi', `
 ┌「 *Donasi • Emoney* 」
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ 
+├ 
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '© icebotz', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
