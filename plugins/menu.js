b7d62ec7e384baf331f6ca58ea995053c8489cd1
wanzbot1 <96588684+wanzbot1@users.noreply.github.com>
2022-01-04 15:53:54 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -383,7 +383,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 }
 handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
-handler.command = /^(menu|help|\?)$/i
+handler.command = /^(menu)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
